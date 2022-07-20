8f6c4fea7287d25ee0bed8afe036b2a7e4db8e8d
Avram Kjiprovski <avramkjiprovski@gmail.com>
2022-07-20 23:35:05 +0200

    render cards dynamically according to data file
    The reason I could not do data.json is because there is a browser security update which does not allow me to without using some kind of third party technology. I will provide some links for this in the readme.md

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # social media cards
  Page for displaying cards with "load more"| functionality.
+
+
+# Issues
+
+## Unable to use local json file in any way.
+
+This is a relatively new update that all modern browsers have. If we try to fetch or import a local json file, it will not work because the browser will block it.
+
+For this exercises we are not allowed to use any third party software, this means bundlers, frameworks, libraries or even nodejs are not allowed.
+
+In order to really make this solution as asked by the employer, we'd either have to use some third party solution, or `--disable-web-security` which is a definite red flag.
+
+All of this considered, I have decided to copy the contents of `data.json` into `data.js`, assign them to a variable and use the variable for all operations in `main.js`.
+
+Here are some links for reference:
+https://stackoverflow.com/questions/4819060/allow-google-chrome-to-use-xmlhttprequest-to-load-a-url-from-a-local-file
+https://stackoverflow.com/questions/43695523/searching-a-local-json-file-not-working-in-chrome
+https://stackoverflow.com/questions/3595515/origin-null-is-not-allowed-by-access-control-allow-origin-error-for-request-ma/3744697#3744697
+https://bugs.chromium.org/p/chromium/issues/detail?id=40787&q=ajax%20local&colspec=ID%20Stars%20Pri%20Area%20Feature%20Type%20Status%20Summary%20Modified%20Owner%20Mstone%20OS
+https://bugs.chromium.org/p/chromium/issues/detail?id=47416
+
+It would be my pleassure if you could share the solution if there is a solution I am not seeing.
+
+## 
--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,90 @@
-import data from './data.json';
+const create_card_header = (profile_image, name, date, source_type) => {
+    const card_header = document.createElement("header");
+    card_header.classList.add("card-header");
+    card_header.innerHTML = `
+    <header class="card-header">
+        <div class="header-avatar-box">
+            <img alt="Avatar" class="avatar">
+            <div class="avatar-text-box">
+                <h2 class="avatar-name"></h2>
+                <p class="avatar-date"></p>
+            </div>
+        </div>
+        <div class="header-icon">
+            <img alt="Icon" class="icon">
+        </div>
+    </header>`
+    card_header.querySelector(".avatar").src = profile_image;
+    card_header.querySelector('.avatar-name').innerText = name;
+    card_header.querySelector('.avatar-date').innerText = date;
+    
+    // choose icon based on source_type string.
+    switch(source_type) {
+        case "facebook": card_header.querySelector('.icon').src = "./assets/facebook.svg"; break;
+        default: card_header.querySelector('.icon').src = "./assets/instagram-logo.svg"; break;
+    }
+    
 
+    return card_header;
+}
+
+const create_card_body = (card_image, caption) => {
+    const card_body = document.createElement('section');
+    card_body.classList.add("card-body");
+    card_body.innerHTML = `
+    <div class="card-image">
+        <img alt="image">
+    </div>
+    <div class="card-content">
+        <p class="content-text"></p>
+    </div>`
+
+    card_body.querySelector('img').src = card_image;
+    card_body.querySelector('.content-text').innerText = caption;
+
+    return card_body;
+}
+
+
+const create_card_footer = (likes) => {
+    const card_footer = document.createElement('footer');
+    card_footer.classList.add("card-footer");
+    card_footer.innerHTML = `
+        <ul class="likes-box">
+            <li><img alt="heart" class="heart"></li>
+            <li class="likes"></li>
+        </ul>`
+    card_footer.querySelector('.heart').src = "./assets/heart.svg";
+    card_footer.querySelector('.likes').innerText = likes;
+
+    return card_footer;
+}
+
+const create_card = (index) => {
+
+    const card_header = create_card_header(data[index].profile_image, data[index].name, data[index].date, data[index].source_type)
+    const card_body = create_card_body(data[index].image, data[index].caption)
+    const card_footer = create_card_footer(data[index].likes)
+
+    const card = document.createElement('article');
+    card.classList.add("card");
+    card.appendChild(card_header);
+    card.appendChild(card_body);
+    card.appendChild(card_footer);
+
+    return card;
+}
+
+const render_cards = (index) => {
+    for(let i = index; i < index+4; i++) {
+        if(i <= data.length) {
+            const card = create_card(i);
+            document.querySelector('.cards-container').appendChild(card);
+        } else {break}
+    }
+
+    return index > data.length ? data.length : index + 4;
+}
+
+console.log(render_cards(0));
+console.log(render_cards(4));
